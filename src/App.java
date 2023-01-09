f7ab6c85c78b7af17d18db644d1515905896d773
LuigiTanaka <luigi.tanaka12@gmail.com>
2023-01-09 18:45:56 -0300

    resolução do exercício

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int x = 16;
+
+        System.out.println("Tabela de multiplicação de " + x);
+
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(x + "x" + i + " = " + x*i);
+        }
     }
 }
